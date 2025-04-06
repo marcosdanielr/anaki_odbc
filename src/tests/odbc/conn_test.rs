5c4d4392e3d9dec9fdf2e138813209dad2142aec
Marcos Roque <devmarcosdanielr@gmail.com>
2025-04-06 16:45:13 -0300

    test: odbc connection

---
+++ src/tests/odbc/conn_test.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    use crate::odbc::conn::OdbcConnection;
+    use std::env;
+
+    #[test]
+    fn test_connect_success() {
+        let odbc_connection = OdbcConnection::new().expect("failed to connect do database");
+
+        let conn_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL not set");
+
+        let result = odbc_connection.connect(conn_url.as_str());
+
+        assert!(result.is_ok(), "Expected Ok(Connection), got {:?}", result);
+    }
+}
